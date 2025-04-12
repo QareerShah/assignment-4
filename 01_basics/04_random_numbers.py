7e4681713d2756ac93da11018a19ec4c33546521
QareerShah <queensyeda16@gmail.com>
2025-04-12 19:22:58 +0500

    basic

---
+++ 01_basics/04_random_numbers.py
@@ -0,0 +1,11 @@
+import random
+
+def main():
+    for _ in range(10):
+        number = random.randint(1, 100)
+        print(number, end=' ')
+    print()  # Newline at the end
+
+# Run the function
+if __name__ == "__main__":
+    main()
